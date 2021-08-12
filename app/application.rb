4e7710e761983060d51461a9f326600714e34401
slicky1 <okunonke@gmail.com>
2021-08-12 03:04:09 -0400

    more routes creations

--- app/application.rb
+++ app/application.rb
@@ -4,19 +4,25 @@ class Application
     res = Rack::Response.new
     req = Rack::Request.new(env)
 # Brand Route
-    if req.path.match('/brand') && req.get?
+    if req.path == ('/brand') && req.get?
       return [200, {'Content-Type' => 'application/json'}, [Brand.all.to_json]]
     end
 
 # Collection Route
-    if req.path.match('/collection') && req.get?
+    if req.path == ('/collection') && req.get?
       return [200, {'Content-Type' => 'application/json'}, [Collection.all.to_json]]
     end
 # Sneaker Route
-    if req.path.match('/sneakers') && req.get?
+    if req.path == ('/sneakers') && req.get?
   return [200, {'Content-Type' => 'application/json'}, [Sneaker.all.to_json]]
     end
+# Create Brand
 
+    if req.path == ('/brand') && req.post?
+      body = JSON.parse(req.body.read)
+      brand = Brand.create(body)
+      return [201, {'Content-Type' => 'application/json'}, [brand.to_json]]
+    end
 # Test Route
     if req.path.match(/test/) 
       return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
