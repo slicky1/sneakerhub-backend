04deeef1d0de2868f8de1502a24b5a3cbf3a942e
slicky1 <okunonke@gmail.com>
2021-08-12 01:51:15 -0400

    routes creations

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,21 @@ class Application
   def call(env)
     res = Rack::Response.new
     req = Rack::Request.new(env)
+# Brand Route
+    if req.path.match('/brand') && req.get?
+      return [200, {'Content-Type' => 'application/json'}, [Brand.all.to_json]]
+    end
 
+# Collection Route
+    if req.path.match('/collection') && req.get?
+      return [200, {'Content-Type' => 'application/json'}, [Collection.all.to_json]]
+    end
+# Sneaker Route
+    if req.path.match('/sneakers') && req.get?
+  return [200, {'Content-Type' => 'application/json'}, [Sneaker.all.to_json]]
+    end
+
+# Test Route
     if req.path.match(/test/) 
       return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
 
