6878dc31b0379c93717f2c9a3861919814b5c8b2
slicky1 <okunonke@gmail.com>
2021-08-12 03:39:38 -0400

    show routes creations

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,9 @@ class Application
   def call(env)
     res = Rack::Response.new
     req = Rack::Request.new(env)
+
+    #MODEL ROUTES
+
 # Brand Route
     if req.path == ('/brand') && req.get?
       return [200, {'Content-Type' => 'application/json'}, [Brand.all.to_json]]
@@ -16,6 +19,8 @@ class Application
     if req.path == ('/sneakers') && req.get?
   return [200, {'Content-Type' => 'application/json'}, [Sneaker.all.to_json]]
     end
+
+    #MODEL CREATION ROUTES
 # Create Brand
 
     if req.path == ('/brand') && req.post?
@@ -23,6 +28,18 @@ class Application
       brand = Brand.create(body)
       return [201, {'Content-Type' => 'application/json'}, [brand.to_json]]
     end
+
+    #MODEL SHOW ROUTES
+    #Show Brand
+    
+    if req.path.match('/brand/') && req.get?
+      id = req.path.split('/') [2]
+      brand = Brand.find(id)
+      return [200, {'Content-Type' => 'application/json'},[brand.to_json]]
+    end
+    #MODEL UPDATES ROUTES
+
+    #MODEL DELETE ROUTES 
 # Test Route
     if req.path.match(/test/) 
       return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
